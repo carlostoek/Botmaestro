71fef49aef96f43d5d1a4696032ebebf82e70813
Chrs <carlostoek@gmail.com>
2025-06-14 20:31:28 -0600

    📦 Added old gamification bot for integration2

--- old_gamificacion/mybot/keyboards/vip_kb.py
+++
@@ -1,7 +0,0 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-
-def get_vip_kb():
-    builder = InlineKeyboardBuilder()
-    builder.button(text="VIP Placeholder", callback_data="vip_placeholder")
-    return builder.as_markup()
